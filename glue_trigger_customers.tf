b676021d87ccd23f89a2d59ab02b39330e94a72a
maher naija <maher.naija@gmail.com>
2022-07-12 17:04:15 +0200

    update

--- glue_trigger_customers.tf
+++ glue_trigger_customers.tf
@@ -7,30 +7,31 @@ resource "aws_glue_trigger" "customers" {
 #description = ""
 enabled = "true"
 #workflow_name = ""
-event_batching_condition {
-  batch_size = ""
-  batch_window =""
-} 
+#event_batching_condition {
+#  batch_size = ""
+#  batch_window =""
+#} 
 
 #SCHEDULED and CONDITIONAL triggers when created. True is not supported for ON_DEMAND triggers.""
-start_on_creation = "true"
+#start_on_creation = "true"
 
 
- type = "CONDITIONAL" # "ON_DEMAND"  "SCHEDULED"
-#  schedule = "cron(15 12 * * ? *)"
+# type = "CONDITIONAL" # "ON_DEMAND"  "SCHEDULED"
+  schedule = "cron(15 12 * * ? *)"
+  type     = "SCHEDULED"
 
   actions {
-    job_name = aws_glue_job.example1.name
+    job_name = aws_glue_job.customers.name
 #arguments
 #timeout
 #security_configuration
-            notification_property {
+#            notification_property {
 #notify_delay_after
-            } 
+#            } 
 #or#    crawler_name = aws_glue_crawler.example1.name
   }
 
-  predicate {
+#  predicate {
 #logical
 #    conditions {
 #      crawler_name = aws_glue_crawler.example2.name
@@ -38,12 +39,12 @@ start_on_creation = "true"
 #logical_operator
 #    }
 
-    conditions {
-      job_name = aws_glue_job.example2.name
-      state    = "SUCCEEDED"
+#    conditions {
+#      job_name = aws_glue_job.example2.name
+#      state    = "SUCCEEDED"
 #logical_operator
-    }
-  }
+#    }
+#  }
 #tags
 
 }
